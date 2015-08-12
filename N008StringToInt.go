188403c78dd875443d01fda91b8de1812de43713
Vince Yuan <vince.yuan@gmail.com>
2015-08-12 13:32:37 +0800

    Added N008StringToInt.

---
+++ N008StringToInt.go
@@ -0,0 +1,85 @@
+package main
+
+const (
+	INT_MAX = 2147483647
+	INT_MIN = -2147483648
+)
+
+type N008StringToInt struct {
+}
+
+func (this *N008StringToInt) myAtoi(str string) int32 {
+	length := len(str)
+	if length == 0 {
+		return 0
+	}
+
+	var value int64 = 0
+	index := 0
+	indexE := -1
+	isNeg := false
+	foundE := false
+	for str[index] == ' ' && index < length {
+		index++
+	}
+	if str[index] == '-' {
+		isNeg = true
+		index++
+	} else if str[index] == '+' {
+		index++
+	}
+	for ; index < length; index++ {
+		if str[index] == 'e' || str[index] == 'E' {
+			foundE = true
+			indexE = index
+			break
+		} else if str[index] >= '0' && str[index] <= '9' {
+			value = value*10 + int64(str[index]) - int64('0')
+			if !isNeg {
+				if value > INT_MAX {
+					return INT_MAX
+				}
+			} else {
+				if -value < INT_MIN {
+					return INT_MIN
+				}
+			}
+		} else {
+			break
+		}
+	}
+
+	var valueAfterE int64 = 0
+	if foundE {
+		for indexE = indexE + 1; indexE < length; indexE++ {
+			valueAfterE = valueAfterE*10 + int64(str[indexE]) - int64('0')
+		}
+	}
+
+	oldValue := value
+	var i int64 = 0
+	for ; i < valueAfterE; i++ {
+		value *= 10
+		if !isNeg {
+			if value > INT_MAX {
+				value = oldValue
+				break
+			}
+		} else {
+			if -value < INT_MIN {
+				value = oldValue
+				break
+			}
+		}
+	}
+	if isNeg {
+		value = -value
+	}
+	if value > INT_MAX {
+		value = INT_MAX
+	}
+	if value < INT_MIN {
+		value = INT_MIN
+	}
+	return int32(value)
+}
---
+++ N008StringToInt_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestN008StringToInt_myAtoi(t *testing.T) {
+	var n008 N008StringToInt
+	fmt.Print("N008StringToInt: ", n008.myAtoi("1"), " ")
+	fmt.Print(n008.myAtoi("0123"), " ")
+	fmt.Print(n008.myAtoi("-1203"), " ")
+	fmt.Print(n008.myAtoi("7e2"), " ")
+	fmt.Print(n008.myAtoi("3E4"), " ")
+	fmt.Print(n008.myAtoi("+11e530408314"), " ")
+	fmt.Print(n008.myAtoi("+-2"), " ")
+	fmt.Print(n008.myAtoi("    010"), " ")
+	fmt.Print(n008.myAtoi("     +004500"), " ")
+	fmt.Print(n008.myAtoi("  -0012a42"), " ")
+	fmt.Print(n008.myAtoi("   +0 123"), " ")
+	fmt.Print(n008.myAtoi("2147483648"), " ")
+	fmt.Print(n008.myAtoi("   -115579378e25"), " ")
+	fmt.Print(n008.myAtoi("9223372036854775809"))
+	fmt.Println()
+}
