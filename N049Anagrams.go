8f0c7fa7fb2935cb1b92ee2a510d69355b89311d
Vince Yuan <vince.yuan@gmail.com>
2015-08-20 00:21:46 +0800

    N049Anagrams: Fixed sorting.

--- N049Anagrams.go
+++ N049Anagrams.go
@@ -2,6 +2,18 @@ package main
 
 import "sort"
 
+type sortBytes []byte
+
+func (s sortBytes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s sortBytes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s sortBytes) Len() int {
+	return len(s)
+}
+
 type N049Anagrams struct {
 }
 
@@ -15,18 +27,19 @@ func (this *N049Anagrams) anagrams(strSlice []string) []string {
 	mymap := make(map[string]int)
 	size := len(strs)
 	for i := 0; i < size; i++ {
-		str := strs[i]
-		sort.Ints(str)
+		str := make([]byte, len(strs[i]))
+		copy(str, strs[i])
+		sort.Sort(sortBytes(str))
 		strString := string(str)
 		v, ok := mymap[strString]
 		if !ok {
 			mymap[strString] = i
 		} else {
 			if v >= 0 {
-				results = append(results, strs[v])
+				results = append(results, string(strs[v]))
 				mymap[strString] = -1 // Avoid adding it more than once
 			}
-			results = append(results, strString)
+			results = append(results, string(strs[i]))
 		}
 	}
 	return results
